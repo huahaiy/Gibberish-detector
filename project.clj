ec2e9dcc1e003584dd287a9630b4a09974b61afc
Steven Landau <slandau@juji-inc.com>
2017-10-17 17:14:33 -0400

    still trying to salvage this

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
-(defproject juji/gibberish-detector "0.1.0-SNAPSHOT"
+(defproject juji/gibberish-detector "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/timbre "4.10.0"]
-                [org.clojure/math.numeric-tower "0.0.4"]
-]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot gibberish-detector.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
