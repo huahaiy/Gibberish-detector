3ba91d5839dff739990582fe261390178596c666
Huahai Yang <huahai.yang@gmail.com>
2022-01-21 10:57:39 -0800

    update

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [trie "0.1.1"]]
-  :main ^:skip-aot gibberish-detector.core
-  :target-path "target/%s"
+  ;:main ^:skip-aot gibberish-detector.core
+  ;:target-path "target/%s"
   :deploy-repositories [["clojars" {:url           "https://repo.clojars.org"
                                     :username      :env/clojars_username
                                     :password      :env/clojars_password
